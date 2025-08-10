39cb3a12415d655b8c246483f93a874f1d2f0d63
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-08-10 10:28:49 -0300

    criei o componente OpcoesHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Logo from "./componentes/Logo";
+import OpcoesHeader from "./componentes/OpcoesHeader";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
+        <OpcoesHeader />
       </header>
     </div>
   );
